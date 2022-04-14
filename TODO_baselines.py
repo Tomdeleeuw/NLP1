5875183ad095b71bf294cec0df444c1ba4f0b2fa
fjorno <70717381+fjorno@users.noreply.github.com>
2022-04-14 22:40:57 +0200

    Ex 10 volledig

--- TODO_baselines.py
+++ TODO_baselines.py
@@ -12,12 +12,12 @@ from itertools import chain
 # Each baseline returns predictions for the test data. The length and frequency baselines determine a threshold using the development data.
 
 def majority_baseline(train_sentences, train_labels, testinput, testlabels):
-    # TODO: determine the majority class based on the training data
+    # determine the majority class based on the training data
     labels = []
     for sentence in train_labels:
         train_tokens = sentence.split(" ")
         for token in train_tokens:
-            token = token.replace('N\n', 'N')
+            token = token.replace('\n', '')
             labels.append(token)
 
     majority_class = str(max(Counter(labels).keys()))
@@ -25,13 +25,12 @@ def majority_baseline(train_sentences, train_labels, testinput, testlabels):
     true_labels = []
     for instance in testlabels:
         tokens = instance.split(" ")
-        tokens = [t.replace('N\n', 'N') for t in tokens]
+        tokens = [t.replace('\n', '') for t in tokens]
         instance_predictions = [majority_class for t in tokens]
         predictions.append(instance_predictions)
-        true_instance = [tokens]
         true_labels.append(tokens)
 
-    # TODO: calculate accuracy for the test input
+    # calculate accuracy for the test input
     predictions = list(chain.from_iterable(predictions))
     true_labels = list(chain.from_iterable(true_labels))
     accuracy = metrics.accuracy_score(true_labels,predictions)
@@ -44,7 +43,7 @@ def random_baseline(train_sentence, train_labels, testinput, testlabels):
     for sentence in train_labels:
         train_tokens = sentence.split(" ")
         for token in train_tokens:
-            token = token.replace('N\n', 'N')
+            token = token.replace('\n', '')
             labels.append(token)
 
     unique_labels = set(labels)
@@ -53,16 +52,14 @@ def random_baseline(train_sentence, train_labels, testinput, testlabels):
     true_labels = []
     for instance in testlabels:
         tokens = instance.split(" ")
-        tokens = [t.replace('N\n', 'N') for t in tokens]
+        tokens = [t.replace('\n', '') for t in tokens]
         instance_predictions = [sample([*unique_labels],1)[0] for t in tokens]
         predictions.append(instance_predictions)
-        true_instance = [tokens]
         true_labels.append(tokens)
 
-    # TODO: calculate accuracy for the test input
-    right_labels = 0
-    total_labels = 0
+    # calculate accuracy for the test input
     accuracy = 0
+
     for k in range(10):
         predictions = list(chain.from_iterable(predictions))
         true_labels = list(chain.from_iterable(true_labels))
@@ -70,6 +67,97 @@ def random_baseline(train_sentence, train_labels, testinput, testlabels):
     accuracy /= 10
 
     return accuracy, predictions
+
+
+def length_baseline(threshold, sentences, labels):
+    true_labels = []
+    for sentence in labels:
+        tokens = sentence.split(" ")
+        tokens = [t.replace('\n', '') for t in tokens]
+        true_labels.append(tokens)
+
+    predictions = []
+    for sentence in sentences:
+        tokens = sentence.split(" ")
+        for token in tokens:
+            token = token.replace('\n', '')
+            if len(token) > threshold:
+                predictions.append("C")
+            else:
+                predictions.append("N")
+
+    true_labels = list(chain.from_iterable(true_labels))
+    accuracy = metrics.accuracy_score(true_labels,predictions)
+
+    return accuracy
+
+
+def frequency_baseline(threshold, sentences, labels):
+    true_labels = []
+    for sentence in labels:
+        tokens = sentence.split(" ")
+        tokens = [t.replace('\n', '') for t in tokens]
+        true_labels.append(tokens)
+
+    words = []
+    for sentence in sentences:
+        tokens = sentence.split(" ")
+        tokens = [t.replace('\n', '') for t in tokens]
+        words.append(tokens)
+    words = list(chain.from_iterable(words))
+    frequencies = Counter(words)
+
+    predictions = []
+    for sentence in sentences:
+        tokens = sentence.split(" ")
+        for token in tokens:
+            token = token.replace('\n', '')
+            if frequencies[token] > threshold:
+                predictions.append("N")
+            else:
+                predictions.append("C")
+
+    true_labels = list(chain.from_iterable(true_labels))
+    accuracy = metrics.accuracy_score(true_labels,predictions)
+
+    return accuracy
+
+
+def optimal_threshold(type, dev_sentences, dev_labels):
+    if type == "length":
+        max_len = 0
+
+        for sentence in dev_sentences:
+            tokens = sentence.split(" ")
+            tokens = [t.replace('\n', '') for t in tokens]
+            if len(max(tokens, key=len)) > max_len:
+                max_len = len(max(tokens, key=len))
+
+        accuracy = 0
+
+        for i in range(max_len):
+            if length_baseline(i, dev_sentences, dev_labels) > accuracy:
+                threshold = i
+                accuracy = length_baseline(i, dev_sentences, dev_labels)
+
+    else:
+        words = []
+
+        for sentence in dev_sentences:
+            tokens = sentence.split(" ")
+            for token in tokens:
+                token = token.replace('\n', '')
+            words.append(token)
+        max_freq = max(Counter(words).values())
+
+        accuracy = 0
+
+        for i in range(max_freq):
+            if frequency_baseline(i, dev_sentences, dev_labels) > accuracy:
+                threshold = i
+                accuracy = frequency_baseline(i, dev_sentences, dev_labels)
+
+    return threshold
 
 
 if __name__ == '__main__':
@@ -88,7 +176,7 @@ if __name__ == '__main__':
     with open(dev_path + "/sentences.txt", encoding='cp1252', errors='ignore') as dev_file:
         dev_sentences = dev_file.readlines()
 
-    with open(train_path + "/labels.txt", encoding='cp1252', errors='ignore') as dev_label_file:
+    with open(dev_path + "/labels.txt", encoding='cp1252', errors='ignore') as dev_label_file:
         dev_labels = dev_label_file.readlines()
 
     with open(test_path + "/sentences.txt", encoding='cp1252', errors='ignore') as testfile:
@@ -97,7 +185,14 @@ if __name__ == '__main__':
     with open(test_path + "/labels.txt", encoding='cp1252', errors='ignore') as test_label_file:
         testlabels = test_label_file.readlines()
 
-    accuracy, predictions = majority_baseline(train_sentences, train_labels, testinput, testlabels)
-    accuracy2, predictions2 = random_baseline(train_sentences, train_labels, testinput, testlabels)
+    accuracy_majority, predictions_majority = majority_baseline(train_sentences, train_labels, testinput, testlabels)
+    accuracy_random, predictions_random = random_baseline(train_sentences, train_labels, testinput, testlabels)
+    threshold_length = optimal_threshold("length", dev_sentences, dev_labels)
+    accuracy_length = length_baseline(threshold_length, testinput, testlabels)
+    threshold_frequency = optimal_threshold("frequency", dev_sentences, dev_labels)
+    accuracy_frequency = frequency_baseline(threshold_frequency, testinput, testlabels)
     # TODO: output the predictions in a suitable way so that you can evaluate them
-    print(accuracy, accuracy2)
+    print("Majority baseline: ", accuracy_majority)
+    print("Random baseline: ", accuracy_random)
+    print("Length baseline: ", threshold_length, accuracy_length)
+    print("Frequency baseline: ", threshold_frequency, accuracy_frequency)
