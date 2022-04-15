d065f654e37ac994fc1c1c6e8998627688dc142b
Tomdeleeuw <tomdeleeuw1@outlook.com>
2022-04-15 16:57:55 +0200

    Update TODO_analyses.py

--- TODO_analyses.py
+++ TODO_analyses.py
@@ -165,6 +165,112 @@ def entities2(doc):
     print(ents)
 
 
+
+# Task 6
+# a) The start offset refers to the index of the first letter of the target word.
+# The end offset referes to the index of the last letter of the target word.
+# If we take for example the following sentence:
+# “The tail of Epidexipteryx also bore unusual vertebrae towards the tip which resembled the feather-anchoring pygostyle of modern birds and some oviraptorosaurs .”,
+# with the target word “Epidexipteryx”.
+# T	h	e		t	a	i	l		o	f
+# 0	1	2	3	4	5	6	7	8	9	10	11
+#
+# E	p	i	i	d	e	x	i	p	t	e	r	y	x		a	l	s	o
+# 12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30
+# In this example the start offset would be 12 and the end offset 25.
+
+# b) The probabilistic label is calculated by: the number of annotators who marked the word as difficult / the total number of annotators.
+# This means that if there were for example 10 native and 10 annotative annotators, we have in total 10 + 10 = 20 annotators.
+# And 2 native and 6 non-native annotators marked the sentence as difficult, in total 2 + 6 = 8 annotators.
+# The probabilistic label would be 8 / 20 = 0.4. This value 0.4 means that 40 percent of all the annotators marked the sentence as difficult.
+
+# c) For the binary label there is no distinction between native or non-native speakers as this is simply not possible to capture in one binary label.
+# If you would like to make a distinction you could make two binary labels, one for native and non-native respectively.
+#
+# For the probabilistic label there is again no differentiation for native and non-native as it simply describes the fraction of annotators that signal the word as difficult.
+# One possibility could be to make a weighted probabilistic label where you would give either the opinion of the native or the non-native annotators more relative importance.
+
+
+def make_task_7_and_8_data():
+    task_7_and_8_data = pd.read_csv('data/original/english/WikiNews_Train.tsv', sep='\t', header=None)
+    task_7_and_8_data['strip_tokens'] = task_7_and_8_data[4].str.replace(r'<[^>]*>', '', regex=True).str.strip()
+    task_7_and_8_data['strip_tokens'] = task_7_and_8_data['strip_tokens'].str.replace('-', ' ', regex=True).str.strip()
+    # task_7_and_8_data['amount_tokens'] = task_7_and_8_data['strip_tokens'].str.split().str.len()
+    task_7_and_8_data['amount_tokens'] = [len(nlp(token)) for token in task_7_and_8_data[4]]
+    return task_7_and_8_data
+
+
+# Task 7
+def extract_basic_statistic():
+    task_7_data = make_task_7_and_8_data()
+    # The binary information
+    print("binary information : ")
+    print(task_7_data[9].value_counts())
+
+    # The probabilistic information
+    print("probabilitic information :")
+    print("minimum = ", task_7_data[10].min())
+    print("maximum = ", task_7_data[10].max())
+    print("median = ", task_7_data[10].median())
+    print("mean = ", task_7_data[10].mean().round(3))
+    print("standard deviation = ", task_7_data[10].std().round(2))
+
+    # The target information
+    print("target word information (False -> <= 1, True -> > 1):")
+    task_7_data['strip_tokens'] = task_7_data[4].str.replace(r'<[^>]*>', '', regex=True).str.strip()
+    task_7_data['amount_tokens'] = task_7_data['strip_tokens'].str.split().str.len()
+    print((task_7_data['amount_tokens'] > 1).value_counts())
+    print("maximum number of tokens = ", task_7_data['amount_tokens'].max())
+
+    return None
+
+
+# Task 8
+def linguistic_characteristics():
+    # Create dataframe
+    task_8_data = make_task_7_and_8_data()
+
+    task_8_data = task_8_data.loc[np.logical_and(task_8_data[9] == 1, task_8_data['amount_tokens'] == 1)]
+    task_8_data.reset_index(inplace=True)
+
+    # Add a column for the number of characters in a token
+    task_8_data["number_characters"] = task_8_data["strip_tokens"].str.len()
+
+    # Add column for word frequency
+    task_8_data["word_frequency"] = pd.Series([word_frequency(word, "en") for word in task_8_data["strip_tokens"]])
+    print(len(task_8_data))
+
+    task_8_data["POS"] = [token.pos_ for sent in task_8_data["strip_tokens"] for token in nlp(sent)]
+
+    # Calculate correlations
+    len_cor = np.corrcoef(task_8_data['number_characters'], task_8_data[10])[0, 1]
+    freq_cor = np.corrcoef(task_8_data['word_frequency'], task_8_data[10])[0, 1]
+    print('correlation between word length and complexity: ', len_cor)
+    print('correlation between word frequency and complexity: ', freq_cor)
+
+    # Create figures
+    fig1 = px.scatter(task_8_data, x='word_frequency', y=10,
+                      labels={'word_frequency': 'Word frequency',
+                              '10': 'Probabilistic complexity'},
+                      title='Probabilistic complexity and word frequency')
+    # fig1.show()
+
+    fig2 = px.scatter(task_8_data, x='number_characters', y=10,
+                      labels={'number_characters': 'Word length',
+                              '10': 'Probabilistic complexity'},
+                      title='Probabilistic complexity and word length')
+    # fig2.show()
+
+    fig3 = px.scatter(task_8_data, x='POS', y=10,
+                      labels={'POS': "POS tag",
+                              '10': 'Probabilistic complexity'},
+                      title='Probabilistic complexity and POS tags')
+    # fig3.show()
+
+    return None
+
+
+
 if __name__ == "__main__":
     with open("data/preprocessed/train/sentences.txt", encoding='cp1252', errors='ignore') as sent_file:
         dataset = sent_file.read()
@@ -173,14 +279,15 @@ if __name__ == "__main__":
     dataset = dataset.replace('\n', ' ')
 
     doc = nlp(dataset)
-    tokenization(doc)
-    data = pos(doc)
-    print(data)
+    # tokenization(doc)
+    # data = pos(doc)
+    # print(data)
     # ngrams(doc,2)
     # ngrams(doc,3)
     # entities(doc)
-    entities2(doc)
+    # entities2(doc)
 
-    selected_lemma, inflections, inflected_sentences = lemmatization(doc)
-    print(selected_lemma, inflections)
-    [print(sen) for sen in inflected_sentences]
+    # selected_lemma, inflections, inflected_sentences = lemmatization(doc)
+    # print(selected_lemma, inflections)
+    # [print(sen) for sen in inflected_sentences]
+    linguistic_characteristics()
